6bd57cb39912783c0e296baa26dc8eb2ee3a3bc8
tenpeoplemeet <tenpeoplemeet@gmail.com>
2014-03-08 11:26:42 +0100

    Remove magic numbers

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+var SECONDS_PER_MINUTE = 60;
+var SECONDS_PER_HOUR = 3600;
+var MILLISECONDS_PER_SECOND = 1000;
+var TIMER_UPDATE_INTERVAL = 1000;
+
 window.onload = init;
 
 // set the date we're counting down to
-var target_date = new Date("Mar 8, 2014").getTime();
+var target_date = new Date("Mar 10, 2014").getTime();
 
 // variables for time units
 var days, hours, minutes, seconds;
@@ -12,7 +17,7 @@ function startAndStopTimer() {
 	var button = document.getElementById("start");
 	var oldText = button.innerHTML;
 	if (oldText == "Start") {
-		t = setInterval(timer, 1000);
+		t = setInterval(timer, TIMER_UPDATE_INTERVAL);
 		button.innerHTML = "Stop";
 	} else {
 		clearInterval(t);
@@ -35,13 +40,13 @@ function timer() {
 
 	// find the amount of "seconds" between now and target
 	var current_date = new Date().getTime();
-	var seconds_left = (target_date - current_date) / 1000;
+	var seconds_left = (target_date - current_date) / MILLISECONDS_PER_SECOND;
 
-	hours = parseInt(seconds_left / 3600);
-	seconds_left = seconds_left % 3600;
+	hours = parseInt(seconds_left / SECONDS_PER_HOUR);
+	seconds_left = seconds_left % SECONDS_PER_HOUR;
 
-	minutes = parseInt(seconds_left / 60);
-	seconds = parseInt(seconds_left % 60);
+	minutes = parseInt(seconds_left / SECONDS_PER_MINUTE);
+	seconds = parseInt(seconds_left % SECONDS_PER_MINUTE);
 
 	// format countdown string + set tag value
 	countdown.innerHTML = hours + "h "
