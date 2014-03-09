e775db1aa6b390da3f53e6956c50ac6a4227d833
tenpeoplemeet <tenpeoplemeet@gmail.com>
2014-03-09 23:30:08 +0100

    Remove log statements

--- script.js
+++ script.js
@@ -13,9 +13,6 @@ Timer.prototype.start = function() {
 	this.targetTime = this.startTime + this.timeRemaining;
 	var t = this;
 	this.intervalID = setInterval(function() { t.update(); }, this.updateIntervalInMs);
-	console.log("start: timer with " + (this.targetTime - this.startTime) / 1000 / 60 + "m");
-	console.log("start: this.targetTime = " + this.targetTime);
-	console.log("start: this.timeRemaining = " + this.timeRemaining);
 };
 
 Timer.prototype.pause = function() {
@@ -25,16 +22,12 @@ Timer.prototype.pause = function() {
 
 Timer.prototype.setTime = function(seconds) {
 	this.timeRemaining = seconds * 1000;
-	console.log("setTime: this.timeRemaining = " + this.timeRemaining);
 };
 
 Timer.prototype.update = function() {
 	var now = new Date().getTime();
 	this.timeRemaining = this.targetTime - now;
 	var secondsRemaining = Math.floor(this.timeRemaining / 1000);
-	console.log("update: this.timeRemaining = " + this.timeRemaining);
-	console.log("update: secondsRemaining = " + this.timeRemaining / 1000);
-	console.log("update: this.targetTime = " + this.targetTime / 1000);
 
 	// How to assign a function to a class property and call it here?
 	// this.updateCallback(secondsRemaining);
@@ -71,5 +64,4 @@ function updateTimerDisplay(secondsRemaining) {
 	var seconds = parseInt(secondsRemaining % 60);
 	var countdown = document.getElementById("countdown");
 	countdown.innerHTML = hours + "h "+ minutes + "m " + seconds + "s";
-	console.log(hours + "h "+ minutes + "m " + seconds + "s");
 };
